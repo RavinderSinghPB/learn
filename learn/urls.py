d450af68d30582d8f64658793863be393f1e5ad1
RavinderSinghPB <code@ParmarthBharat.com>
2023-03-16 15:48:23 +0530

    html str in viewfn

--- learn/urls.py
+++ learn/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index,home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home',home),
+    path('',index)
+
 ]
